e0ffb9228a694f167d2a83ae6faae646de65dd69
horiem <yellowshippo@gmail.com>
2024-04-19 00:30:48 +0900

    add tests

--- tests/test_tensors/test__dimensions.py
+++ tests/test_tensors/test__dimensions.py
@@ -1,6 +1,9 @@
 import pytest
 
+import torch
+
 from phlower import PhlowerDimensionTensor
+from phlower.utils.exceptions import DimensionIncompatibleError
 
 
 @pytest.mark.parametrize(
@@ -28,3 +31,42 @@ def test__add(unit1, unit2):
     unit2 = PhlowerDimensionTensor.from_list(unit2)
 
     assert unit1 == (unit1 + unit2)
+
+
+@pytest.mark.parametrize(
+    "unit",
+    [
+        [0, 2, 0, 0, 0, 0, 0],
+        [1, 2, 0, 2, 0, 0, 0],
+    ],
+)
+@pytest.mark.parametrize(
+    "dim",
+    [0, 2]
+)
+def test__cat(unit, dim):
+    unit1 = PhlowerDimensionTensor.from_list(unit)
+    unit2 = PhlowerDimensionTensor.from_list(unit)
+
+    unit1 == torch.cat([unit1, unit2], dim=dim)
+
+
+@pytest.mark.parametrize(
+    "unit1, unit2",
+    [
+        (
+            [0, 2, 0, 0, 0, 0, 0],
+            [0, 2, 1, 0, 0, 0, 0],
+        ),
+        (
+            [1, 2, 0, 2, 0, 0, 0],
+            [1, 2, -1, 2, 0, 0, 0],
+        ),
+    ],
+)
+def test__cat_raise_dimension_incompatible(unit1, unit2):
+    unit1 = PhlowerDimensionTensor.from_list(unit1)
+    unit2 = PhlowerDimensionTensor.from_list(unit2)
+
+    with pytest.raises(DimensionIncompatibleError):
+        torch.cat([unit1, unit2], dim=0)
