e67d9ae2d3ccaa8daba115712387aded45cf561a
horiem <yellowshippo@gmail.com>
2024-08-08 13:38:11 +0900

    start adding en equivariant mlp

---
+++ src/phlower/nn/_core_modules/_en_equivariant_mlp.py
@@ -0,0 +1,77 @@
+from __future__ import annotations
+
+import torch
+from typing_extensions import Self
+
+from phlower._base.tensors import PhlowerTensor
+from phlower.collections.tensors import IPhlowerTensorCollections
+from phlower.nn._core_modules import _utils
+from phlower.nn._interface_module import IPhlowerCoreModule
+from phlower.settings._module_settings import MLPSetting
+
+
+class EnEquivariantMLP(IPhlowerCoreModule, torch.nn.Module):
+    """E(n)-equivariant Multi Layer Perceptron"""
+
+    @classmethod
+    def from_setting(cls, setting: MLPSetting) -> Self:
+        """Generate MLP from setting object
+
+        Args:
+            setting (MLPSetting): setting object
+
+        Returns:
+            Self: MLP object
+        """
+        return EnEquivariantMLP(**setting.__dict__)
+
+    @classmethod
+    def get_nn_name(cls) -> str:
+        """Return neural network name
+
+        Returns:
+            str: name
+        """
+        return "EnEquivariantMLP"
+
+    def __init__(
+        self,
+        nodes: list[int],
+        activations: list[str] | None = None,
+        dropouts: list[float] | None = None,
+        bias: bool = False,
+    ) -> None:
+        super().__init__()
+
+        if activations is None:
+            activations = []
+        if dropouts is None:
+            dropouts = []
+
+        self._chains = _utils.ExtendedLinearList(
+            nodes=nodes, activations=activations, dropouts=dropouts, bias=bias
+        )
+        self._nodes = nodes
+        self._activations = activations
+
+    def forward(
+        self,
+        data: IPhlowerTensorCollections,
+        *,
+        supports: dict[str, PhlowerTensor] | None = None,
+    ) -> PhlowerTensor:
+        """forward function which overloads torch.nn.Module
+
+        Args:
+            data (IPhlowerTensorCollections):
+                data which receives from predecessors
+            supports (dict[str, PhlowerTensor], optional):
+                Graph object. Defaults to None.
+
+        Returns:
+            PhlowerTensor: Tensor object
+        """
+        h = data.unique_item()
+        for i in range(len(self._chains)):
+            h = self._chains.forward(h, index=i)
+        return h
