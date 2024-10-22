7f2af21884c60542e4aba479b941f6dbf71e3c07
horiem <yellowshippo@gmail.com>
2024-10-23 00:54:38 +0900

    update gpu tests to eliminate cuda in fixtures

--- tests/gpu_tests/test_base/test_tensors/test_phlower_tensor_gpu.py
+++ tests/gpu_tests/test_base/test_tensors/test_phlower_tensor_gpu.py
@@ -4,21 +4,25 @@ import torch
 from phlower import PhlowerTensor, phlower_tensor
 
 
-def generate_random_phlower_tensor_on_gpu(
+def generate_random_phlower_tensor(
     has_dimension: bool = True,
 ) -> PhlowerTensor:
     if has_dimension:
         dimension = {"L": 2, "T": -1}
     else:
         dimension = {}
-    return phlower_tensor(torch.rand(5), dimension=dimension).to(
-        torch.device("cuda:0")
-    )
+    return phlower_tensor(torch.rand(5), dimension=dimension)
 
 
-def to_tensor_if_needed(x: PhlowerTensor | float) -> torch.Tensor:
+def to_tensor_if_needed(x: PhlowerTensor | float) -> torch.Tensor | float:
     if isinstance(x, PhlowerTensor):
         return x.to_tensor()
+    return x
+
+
+def to_cuda_if_needed(x: PhlowerTensor | float) -> PhlowerTensor | float:
+    if isinstance(x, PhlowerTensor):
+        return x.to('cuda:0')
     return x
 
 
@@ -31,15 +35,15 @@ def to_tensor_if_needed(x: PhlowerTensor | float) -> torch.Tensor:
     "a, b",
     [
         (
-            generate_random_phlower_tensor_on_gpu(),
-            generate_random_phlower_tensor_on_gpu(),
+            generate_random_phlower_tensor(),
+            generate_random_phlower_tensor(),
         ),
     ],
 )
 def test__op_tensor_tensor_with_unit_on_gpu(
     op: callable, a: PhlowerTensor | float, b: PhlowerTensor | float
 ):
-    c = op(a, b)
+    c = op(to_cuda_if_needed(a), to_cuda_if_needed(b))
 
     tc = op(to_tensor_if_needed(a), to_tensor_if_needed(b))
     np.testing.assert_almost_equal(c.numpy(), tc.cpu().numpy())
@@ -57,10 +61,10 @@ def test__op_tensor_tensor_with_unit_on_gpu(
     [
         (
             2.3,
-            generate_random_phlower_tensor_on_gpu(),
+            generate_random_phlower_tensor(),
         ),
         (
-            generate_random_phlower_tensor_on_gpu(),
+            generate_random_phlower_tensor(),
             2.3,
         ),
     ],
@@ -68,7 +72,7 @@ def test__op_tensor_tensor_with_unit_on_gpu(
 def test__op_tensor_scalar_with_unit_on_gpu(
     op: callable, a: PhlowerTensor | float, b: PhlowerTensor | float
 ):
-    c = op(a, b)
+    c = op(to_cuda_if_needed(a), to_cuda_if_needed(b))
 
     tc = op(to_tensor_if_needed(a), to_tensor_if_needed(b))
     np.testing.assert_almost_equal(c.numpy(), tc.cpu().numpy())
@@ -85,15 +89,15 @@ def test__op_tensor_scalar_with_unit_on_gpu(
     "a, b",
     [
         (
-            generate_random_phlower_tensor_on_gpu(has_dimension=False),
-            generate_random_phlower_tensor_on_gpu(has_dimension=False),
+            generate_random_phlower_tensor(has_dimension=False),
+            generate_random_phlower_tensor(has_dimension=False),
         ),
         (
             2.3,
-            generate_random_phlower_tensor_on_gpu(has_dimension=False),
+            generate_random_phlower_tensor(has_dimension=False),
         ),
         (
-            generate_random_phlower_tensor_on_gpu(has_dimension=False),
+            generate_random_phlower_tensor(has_dimension=False),
             2.3,
         ),
     ],
@@ -101,7 +105,7 @@ def test__op_tensor_scalar_with_unit_on_gpu(
 def test__op_tensor_tensor_without_unit_on_gpu(
     op: callable, a: PhlowerTensor | float, b: PhlowerTensor | float
 ):
-    c = op(a, b)
+    c = op(to_cuda_if_needed(a), to_cuda_if_needed(b))
 
     tc = op(to_tensor_if_needed(a), to_tensor_if_needed(b))
     np.testing.assert_almost_equal(c.numpy(), tc.cpu().numpy())
