6fc7e1dd64283502150830f2c713ea30d4929be0
sakamoto <sakamoto@ricos.co.jp>
2024-12-05 18:03:26 +0900

    [refactoring] divide setting files for group and module setting

--- src/phlower/settings/_group_setting.py
+++ src/phlower/settings/_group_setting.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 from typing import Literal
 
-import dagstream
 import pydantic
 from dagstream.utils.errors import DagStreamCycleError
 from pydantic import Field
@@ -14,8 +13,9 @@ from phlower.settings._interface import (
     IPhlowerLayerParameters,
     IReadOnlyReferenceGroupSetting,
 )
-from phlower.settings._module_parameter_setting import PhlowerModuleParameters
+from phlower.settings._module_setting import ModuleSetting
 from phlower.settings._nonlinear_solver_setting import SolverParameters
+from phlower.settings._resolver import resolve_modules
 from phlower.utils.exceptions import (
     PhlowerModuleCycleError,
     PhlowerModuleDuplicateKeyError,
@@ -110,7 +110,7 @@ class GroupModuleSetting(
             else:
                 type_v = getattr(v, "nn_type", None)
 
-            if type_v == "GROUP":
+            if (type_v == "GROUP") or (type_v == "Group"):
                 parsed_items.append(GroupModuleSetting(**v))
             else:
                 parsed_items.append(ModuleSetting(**v))
@@ -118,8 +118,11 @@ class GroupModuleSetting(
         return parsed_items
 
     @pydantic.model_validator(mode="after")
-    def _check_duplicate_name(self) -> Self:
-        _names: set[str] = set()
+    def _check_duplicate_module_name(self) -> Self:
+        _names: set[str] = {v.name for v in self.modules}
+        if len(_names) == len(self.modules):
+            return self
+
         for module in self.modules:
             if module.get_name() in _names:
                 raise PhlowerModuleDuplicateNameError(
@@ -127,7 +130,8 @@ class GroupModuleSetting(
                     f"in {self.name}"
                 )
 
-        return self
+        # it is not supposed to be reached here
+        raise NotImplementedError()
 
     @pydantic.model_validator(mode="after")
     def check_solver_target_exists_in_inputs_and_outputs(self) -> Self:
@@ -179,7 +183,7 @@ class GroupModuleSetting(
         input_info = {v.name: v.n_last_dim for v in self.inputs}
 
         try:
-            results = _resolve_modules(input_info, self.modules, self)
+            results = resolve_modules(input_info, self.modules, self)
         except DagStreamCycleError as ex:
             raise PhlowerModuleCycleError(
                 f"A cycle is detected in {self.name}."
@@ -256,175 +260,3 @@ class GroupModuleSetting(
                     f"is {self_output.n_last_dim}. "
                     "It is not consistent with the precedent modules"
                 )
-
-
-class ModuleSetting(IModuleSetting, pydantic.BaseModel):
-    nn_type: str = Field(..., frozen=True)
-    """
-    name of neural network type. For example, GCN, MLP.
-    """
-
-    name: str = Field(..., frozen=True)
-    """
-    name of group
-    """
-
-    input_keys: list[str] = Field(..., frozen=True)
-    """
-    key names of input variables
-    """
-
-    output_key: str = Field(..., frozen=True)
-    """
-    key names of output variables
-    """
-
-    destinations: list[str] = Field(
-        ..., default_factory=lambda: [], frozen=True
-    )
-    """
-    name of destination modules.
-    """
-
-    no_grad: bool = Field(False, frozen=True)
-    """
-    A Flag not to calculate gradient. Defauls to False.
-    """
-
-    nn_parameters: PhlowerModuleParameters = Field(
-        default_factory=dict, validate_default=True
-    )
-    """
-    parameters for neural networks.
-    Allowed items depend on `nn_type`.
-    """
-
-    # special keyward to forbid extra fields in pydantic
-    model_config = pydantic.ConfigDict(
-        extra="forbid", arbitrary_types_allowed=True
-    )
-
-    def get_name(self) -> str:
-        return self.name
-
-    def get_input_keys(self) -> list[str]:
-        return self.input_keys
-
-    def get_destinations(self) -> list[str]:
-        return self.destinations
-
-    def get_output_info(self) -> dict[str, int]:
-        return {self.output_key: self.nn_parameters.get_n_nodes()[-1]}
-
-    def resolve(
-        self,
-        *resolved_outputs: dict[str, int],
-        parent: IReadOnlyReferenceGroupSetting | None = None,
-    ) -> None:
-        self._check_keys(*resolved_outputs)
-
-        if self.nn_parameters.need_reference:
-            self.nn_parameters.get_reference(parent)
-
-        _resolved_nodes = self._resolve_nodes(*resolved_outputs)
-        # NOTE: overwrite nodes
-        self.nn_parameters.overwrite_nodes(_resolved_nodes)
-        self.nn_parameters.confirm(self)
-
-    def _check_keys(self, *resolved_outputs: dict[str, int]) -> None:
-        _to_input_keys: set[str] = set()
-        n_keys: int = 0
-
-        for output in resolved_outputs:
-            _to_input_keys.update(output.keys())
-            n_keys += len(output)
-
-        if len(_to_input_keys) != n_keys:
-            raise PhlowerModuleDuplicateKeyError(
-                "Duplicate key name is detected in input keys for "
-                f"{self.name}. Please check precedents"
-            )
-
-        for input_key in self.input_keys:
-            if input_key not in _to_input_keys:
-                raise PhlowerModuleKeyError(
-                    f"{input_key} is not passed to {self.name}. "
-                    "Please check precedents."
-                )
-
-    def _resolve_nodes(self, *resolved_outputs: dict[str, int]) -> list[int]:
-        key2node: dict[str, int] = {}
-        for output in resolved_outputs:
-            key2node.update(output)
-
-        try:
-            first_node = self.nn_parameters.gather_input_dims(
-                *(key2node[key] for key in self.input_keys)
-            )
-        except ValueError as ex:
-            raise PhlowerModuleNodeDimSizeError(
-                f"inputs for {self.name} have problems"
-            ) from ex
-
-        nodes = self.nn_parameters.get_n_nodes()
-        if nodes is None:
-            return [first_node, first_node]
-
-        if (nodes[0] != -1) and (nodes[0] != first_node):
-            raise PhlowerModuleNodeDimSizeError(
-                f"nodes in {self.name} is {nodes}. "
-                "It is not consistent with the precedent modules"
-            )
-
-        assert len(nodes) > 1
-        if nodes[-1] == -1:
-            raise PhlowerModuleNodeDimSizeError(
-                "last number of nodes is must be "
-                "integer number which is larger than 0."
-                f" {self.name}"
-            )
-
-        new_nodes = [first_node]
-        new_nodes.extend(nodes[1:])
-        return new_nodes
-
-
-class _SettingResolverAdapter:
-    def __init__(self, setting: IModuleSetting) -> None:
-        self._setting = setting
-
-    @property
-    def name(self) -> str:
-        return self._setting.get_name()
-
-    def __call__(
-        self,
-        *resolved_output: dict[str, int],
-        parent: IReadOnlyReferenceGroupSetting | None = None,
-    ) -> int:
-        self._setting.resolve(*resolved_output, parent=parent)
-        return self._setting.get_output_info()
-
-
-def _resolve_modules(
-    starts: dict[str, int],
-    modules: list[IModuleSetting],
-    parent: IReadOnlyReferenceGroupSetting | None = None,
-) -> list[dict[str, int]]:
-    stream = dagstream.DagStream()
-    resolvers = [_SettingResolverAdapter(layer) for layer in modules]
-    name2node = {
-        _resolver.name: stream.emplace(_resolver)[0] for _resolver in resolvers
-    }
-
-    for layer in modules:
-        node = name2node[layer.get_name()]
-        for to_name in layer.get_destinations():
-            node.precede(name2node[to_name], pipe=True)
-
-    _dag = stream.construct()
-
-    executor = dagstream.StreamExecutor(_dag)
-    results = executor.run(parent=parent, first_args=(starts,))
-
-    return list(results.values())
