f309098113c45bb415acda1fd7b145ef6a64d984
sakamoto <sakamoto@ricos.co.jp>
2024-12-12 22:41:58 +0900

    [fix] fix lint errors

--- src/phlower/settings/_module_settings/__init__.py
+++ src/phlower/settings/_module_settings/__init__.py
@@ -6,11 +6,11 @@ from phlower.settings._module_settings._accessor_setting import AccessorSetting
 from phlower.settings._module_settings._concatenator_setting import (
     ConcatenatorSetting,
 )
-from phlower.settings._module_settings._dirichlet_setting import (
-    DirichletSetting,
-)
 from phlower.settings._module_settings._contraction_setting import (
     ContractionSetting,
+)
+from phlower.settings._module_settings._dirichlet_setting import (
+    DirichletSetting,
 )
 from phlower.settings._module_settings._einsum_setting import EinsumSetting
 from phlower.settings._module_settings._en_equivariant_mlp_setting import (
