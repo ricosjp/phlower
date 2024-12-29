3f0aa43ad09514ec7448419dc1edbf67539815f5
sakamoto <sakamoto@ricos.co.jp>
2024-12-29 21:25:06 +0900

    [improve] add tests for tensor with physical dimension in DeepSets

--- tests/test_nn/test_core_modules/test_deepsets.py
+++ tests/test_nn/test_core_modules/test_deepsets.py
@@ -3,9 +3,11 @@ from collections.abc import Callable
 import hypothesis.strategies as st
 import numpy as np
 import pytest
+import torch
 from hypothesis import given
 from phlower import PhlowerTensor, phlower_tensor
-from phlower._base._functionals import to_batch, unbatch
+from phlower._base import phlower_dimension_tensor
+from phlower._base._functionals import is_same_dimensions, to_batch, unbatch
 from phlower._fields import SimulationField
 from phlower.collections import phlower_tensor_collection
 from phlower.nn import DeepSets, MLPConfiguration
@@ -93,13 +95,23 @@ def test__can_pass_parameters_via_setting(
 @pytest.mark.parametrize(
     "nodes, activations, bias, last_activation_name, pool_operator_name",
     [
-        ([-1, 10, 5], ["tanh", "tanh"], True, "relu", "max"),
+        ([-1, 10, 5], ["relu", "relu"], True, "relu", "max"),
         ([-1, 10, 20], ["relu", "identity"], True, "tanh", "mean"),
     ],
 )
 @pytest.mark.parametrize(
-    "input_shape",
-    [(10, 3, 1), (20, 8), (5, 20, 2, 1)],
+    "input_shape, is_time_series",
+    [
+        ((10, 3, 1), False),
+        ((20, 8), False),
+        ((5, 20, 2, 1), False),
+        ((2, 10, 3, 1), True),
+        ((1, 20, 6), True),
+        ((5, 100, 5), True),
+    ],
+)
+@pytest.mark.parametrize(
+    "dimension", [{"Theta": 1}, {"M": 1, "T": -1}, None, {}]
 )
 def test__permutation_equivariance_for_vertexwise_tensor(
     nodes: list[int],
@@ -108,12 +120,15 @@ def test__permutation_equivariance_for_vertexwise_tensor(
     last_activation_name: str,
     pool_operator_name: str,
     input_shape: tuple[int],
+    is_time_series: bool,
+    dimension: dict | None,
 ):
     nodes[0] = input_shape[-1]
-    _tensor = phlower_tensor(np.random.rand(*input_shape))
-    n_nodes = _tensor.shape_pattern.get_n_vertices()
-    permute_idxes = np.random.permutation(n_nodes)
-    _permuted_tensor = _tensor[permute_idxes, ...]
+    _tensor = phlower_tensor(
+        np.random.rand(*input_shape),
+        is_time_series=is_time_series,
+        dimension=dimension,
+    )
 
     config = MLPConfiguration(
         nodes=nodes, activations=activations, dropouts=[], bias=bias
@@ -128,42 +143,76 @@ def test__permutation_equivariance_for_vertexwise_tensor(
     result1 = model.forward(phlower_tensor_collection({"input": _tensor}))
     assert len(result1.shape) == len(input_shape)
     assert result1.shape[-1] == nodes[-1]
+    if dimension is not None:
+        assert (
+            result1.dimension.to_dict()
+            == phlower_dimension_tensor(dimension).to_dict()
+        )
+    else:
+        assert result1.dimension is None
 
+    n_nodes = _tensor.shape_pattern.get_n_vertices()
+    permute_idxes = np.random.permutation(n_nodes)
+
+    # Permutation along node axis
+    def _random_permutation(
+        value: PhlowerTensor, indexes: np.ndarray
+    ) -> torch.Tensor:
+        if is_time_series:
+            return value[:, indexes, ...]
+        return value[indexes, ...]
+
+    _permuted_tensor = phlower_tensor(
+        _random_permutation(_tensor, permute_idxes),
+        dimension=dimension,
+        is_time_series=is_time_series,
+    )
     result2 = model.forward(
         phlower_tensor_collection({"input": _permuted_tensor})
     )
 
     np.testing.assert_array_almost_equal(
-        result1[permute_idxes].detach().numpy(),
-        result2.to_tensor().detach().numpy(),
+        _random_permutation(result1, permute_idxes).detach().numpy(),
+        result2.to_numpy(),
     )
+
+    assert is_same_dimensions([result1, result2])
 
 
 @pytest.mark.parametrize(
     "nodes, activations, bias, last_activation_name, pool_operator_name",
     [
-        ([-1, 10, 5], ["tanh", "tanh"], True, "relu", "max"),
-        ([-1, 10, 20], ["relu", "identity"], True, "tanh", "mean"),
+        ([-1, 10, 5], ["tanh", "tanh"], True, "tanh", "max"),
+        ([-1, 10, 20], ["relu", "tanh"], True, "relu", "mean"),
     ],
 )
 @pytest.mark.parametrize(
-    "input_shape",
-    [(2, 10, 3, 1), (1, 20, 6), (5, 100, 5)],
+    "input_shape, is_time_series",
+    [
+        ((10, 3, 1), False),
+        ((20, 8), False),
+        ((5, 20, 2, 1), False),
+        ((2, 10, 3, 1), True),
+        ((1, 20, 6), True),
+        ((5, 100, 5), True),
+    ],
 )
-def test__permutation_equivariance_for_time_series_tensor(
+@pytest.mark.parametrize("dimension", [{}, None])
+def test__permutation_equivariance_with_non_dimension(
     nodes: list[int],
     activations: list[str],
     bias: bool,
     last_activation_name: str,
     pool_operator_name: str,
     input_shape: tuple[int],
+    is_time_series: bool,
+    dimension: dict | None,
 ):
     nodes[0] = input_shape[-1]
-    _tensor = phlower_tensor(np.random.rand(*input_shape), is_time_series=True)
-    n_nodes = _tensor.shape_pattern.get_n_vertices()
-    permute_idxes = np.random.permutation(n_nodes)
-    _permuted_tensor = phlower_tensor(
-        _tensor[:, permute_idxes, ...], is_time_series=True
+    _tensor = phlower_tensor(
+        np.random.rand(*input_shape),
+        is_time_series=is_time_series,
+        dimension=dimension,
     )
 
     config = MLPConfiguration(
@@ -179,16 +228,40 @@ def test__permutation_equivariance_for_time_series_tensor(
     result1 = model.forward(phlower_tensor_collection({"input": _tensor}))
     assert len(result1.shape) == len(input_shape)
     assert result1.shape[-1] == nodes[-1]
-    assert result1.is_time_series
+    if dimension is not None:
+        assert (
+            result1.dimension.to_dict()
+            == phlower_dimension_tensor(dimension).to_dict()
+        )
+    else:
+        assert result1.dimension is None
 
+    n_nodes = _tensor.shape_pattern.get_n_vertices()
+    permute_idxes = np.random.permutation(n_nodes)
+
+    # Permutation along node axis
+    def _random_permutation(
+        value: PhlowerTensor, indexes: np.ndarray
+    ) -> torch.Tensor:
+        if is_time_series:
+            return value[:, indexes, ...]
+        return value[indexes, ...]
+
+    _permuted_tensor = phlower_tensor(
+        _random_permutation(_tensor, permute_idxes),
+        dimension=dimension,
+        is_time_series=is_time_series,
+    )
     result2 = model.forward(
         phlower_tensor_collection({"input": _permuted_tensor})
     )
 
     np.testing.assert_array_almost_equal(
-        result1[:, permute_idxes, ...].detach().numpy(),
-        result2.to_tensor().detach().numpy(),
+        _random_permutation(result1, permute_idxes).detach().numpy(),
+        result2.to_numpy(),
     )
+
+    assert is_same_dimensions([result1, result2])
 
 
 @pytest.mark.parametrize(
