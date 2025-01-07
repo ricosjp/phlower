aeaf17fa151b828dad3c75ec2ecd8753ba79ebbd
sakamoto <sakamoto@ricos.co.jp>
2025-01-07 17:48:25 +0900

    [improve] delete unnecessary codes. add tests for sparse array wrapper

--- src/phlower/_base/array/sparse/_sparse_array_wrapper.py
+++ src/phlower/_base/array/sparse/_sparse_array_wrapper.py
@@ -1,12 +1,10 @@
 from __future__ import annotations
 
-from collections.abc import Callable, Sequence
+from collections.abc import Callable
 
 import numpy as np
-import scipy.sparse as sp
 import torch
 
-from phlower._base._batch import GraphBatchInfo
 from phlower._base._dimension import PhysicalDimensions
 from phlower._base.array._interface_wrapper import IPhlowerArray
 from phlower._base.tensors import PhlowerTensor, phlower_tensor
@@ -69,7 +67,7 @@ class SparseArrayWrapper(IPhlowerArray):
         if use_diagonal:
             raise ValueError(
                 "Cannot set use_diagonal=True in self.apply function. "
-                "use_diagonal is only allows in self.reshape"
+                "use_diagonal is only allows in `reshape` method"
             )
 
         reshaped_array = self.reshape(
@@ -130,77 +128,3 @@ class SparseArrayWrapper(IPhlowerArray):
         raise ValueError(
             "slice_along_time_axis is not allowed for sparse array."
         )
-
-
-def _sparse_array_batch(
-    arrays: Sequence[SparseArrayWrapper],
-) -> tuple[SparseArrayWrapper, GraphBatchInfo]:
-    concat_arr = _sparse_concatenate(arrays)
-
-    concat_shapes = [arr.shape for arr in arrays]
-    concat_sizes = [arr.size for arr in arrays]
-
-    info = GraphBatchInfo(
-        sizes=concat_sizes,
-        shapes=concat_shapes,
-        n_nodes=tuple(arr.shape[0] for arr in arrays),
-    )
-    return SparseArrayWrapper(concat_arr), info
-
-
-def _sparse_array_unbatch(
-    array: SparseArrayWrapper, batch_info: GraphBatchInfo
-) -> list[SparseArrayWrapper]:
-    results = _sparse_decompose(array.to_numpy(), batch_info)
-    return [SparseArrayWrapper(arr) for arr in results]
-
-
-def _sparse_concatenate(
-    arrays: Sequence[SparseArrayWrapper],
-) -> SparseArrayWrapper:
-    offsets = np.cumsum(
-        np.array(
-            [
-                arrays[i - 1].shape if i != 0 else (0, 0)
-                for i in range(len(arrays))
-            ]
-        ),
-        axis=0,
-    )
-
-    shape = np.sum([arr.shape for arr in arrays], axis=0)
-
-    rows = np.concatenate(
-        [arr.row + offsets[i, 0] for i, arr in enumerate(arrays)],
-        dtype=np.int32,
-    )
-    cols = np.concatenate(
-        [arr.col + offsets[i, 1] for i, arr in enumerate(arrays)],
-        dtype=np.int32,
-    )
-    data = np.concatenate([arr.data for arr in arrays], dtype=np.float32)
-
-    sparse_arr = sp.coo_matrix((data, (rows, cols)), shape=shape)
-    return sparse_arr
-
-
-def _sparse_decompose(
-    array: SparseArrayType, batch_info: GraphBatchInfo
-) -> list[sp.coo_matrix]:
-    sizes = np.cumsum(batch_info.sizes)
-    offsets = np.cumsum(
-        np.array([[0, 0]] + batch_info.shapes[:-1], dtype=np.int32), axis=0
-    )
-
-    rows = np.split(array.row, sizes)
-    cols = np.split(array.col, sizes)
-    data = np.split(array.data, sizes)
-
-    results = [
-        sp.coo_matrix(
-            (data[i], (rows[i] - offsets[i][0], cols[i] - offsets[i][1])),
-            shape=batch_info.shapes[i],
-        )
-        for i in range(len(batch_info))
-    ]
-    return results
