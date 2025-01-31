988ef1a5ef7c0093833c5476ef86585fd03da5b6
horiem <yellowshippo@gmail.com>
2025-01-31 23:05:15 +0900

    lint

--- src/phlower/_base/tensors/_phlower_tensor.py
+++ src/phlower/_base/tensors/_phlower_tensor.py
@@ -493,7 +493,8 @@ class PhlowerTensor(IPhlowerTensor):
         ret: torch.Tensor = func(*_tensors, **kwargs)
 
         tensor_args = [
-            a for a in args
+            a
+            for a in args
             if isinstance(a, PhlowerTensor)
             or (isinstance(a, list | tuple) and isinstance(a[0], PhlowerTensor))
         ]
@@ -501,9 +502,7 @@ class PhlowerTensor(IPhlowerTensor):
             classes = [a.__class__ for a in args]
             raise ValueError(f"Invalid input classes: {classes}")
 
-        list_is_time_series = _recursive_resolve(
-            tensor_args, "is_time_series"
-        )
+        list_is_time_series = _recursive_resolve(tensor_args, "is_time_series")
         list_is_voxel = _recursive_resolve(tensor_args, "is_voxel")
         is_time_series = np.any(list_is_time_series)
         is_voxel = np.any(list_is_voxel)
