011c49ec0fd7d84a0314f7e9d8535162fbaafb0f
sakamoto <sakamoto@ricos.co.jp>
2025-02-12 15:02:02 +0900

    [add] allow user handlers

--- src/phlower/services/trainer/handlers/__init__.py
+++ src/phlower/services/trainer/handlers/__init__.py
@@ -6,6 +6,15 @@ _REGISTERED: dict[str, type[IHandlerCall]] = {
 }
 
 
+def register_user_handler(
+    name: str, handler: IHandlerCall, force_update: bool = False
+) -> None:
+    if (name in _REGISTERED) and (not force_update):
+        raise ValueError(f"{name} has already existed.")
+
+    _REGISTERED[name] = handler
+
+
 def create_handler(name: str, params: dict) -> IHandlerCall:
     if name not in _REGISTERED:
         raise NotImplementedError(f"Handler {name} is not implemented.")
